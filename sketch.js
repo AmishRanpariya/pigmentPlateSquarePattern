6ac86bf4355ab84529e8bec2a626bfc82a9a9b7a
amishranpariya <amishranpariya@gmail.com>
2021-08-20 17:03:32 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,82 @@
+let l = 50;
+let rows, cols;
+const params = new URLSearchParams(window.location.search);
+let colString;
+if (params.has("c")) {
+	colString = params.get("c");
+} else {
+	colString = "000000ffffff000000ffffff";
+}
+function setup() {
+	let canvas = createCanvas(400, 400);
+	cols = width / l;
+	rows = height / l;
+	canvas.mousePressed(stopr);
+
+	noStroke();
+	fill(0, 255, 100);
+	background(20);
+
+	for (let j = 0; j < cols; j++) {
+		for (let i = 0; i < rows; i++) {
+			randomFill("");
+			rect(i * l, j * l, l, l);
+			randomFill();
+			randomShape(i * l, j * l, l, l);
+		}
+	}
+}
+function stopr() {
+	noLoop();
+	saveCanvas(canvas, "TrouchetTiles");
+}
+function randomFill() {
+	let colorHex = colString;
+	let code = random([
+		colorHex.slice(0, 6),
+		colorHex.slice(6, 12),
+		colorHex.slice(12, 18),
+		colorHex.slice(18, 24),
+	]);
+	fill("#" + code);
+	return code;
+}
+
+function chooseDiffThen(oldCol) {
+	while (randomFill() === oldCol) {}
+}
+
+function draw() {
+	randomFill();
+
+	randomShape(floor(random(cols)) * l, floor(random(rows)) * l, l, l);
+	noLoop();
+}
+
+function randomPI() {
+	return random([0, PI / 2, (3 * PI) / 2]);
+}
+function randomShape(x, y, w, h, colString) {
+	let r = random(0, 1);
+	if (r < 0.25) {
+		let bgCol = randomFill();
+		arc(x + w, y + h, w * 2, h * 2, PI, (3 * PI) / 2); //bg
+		chooseDiffThen(bgCol);
+		arc(x, y, w * 2, h * 2, 0, PI / 2);
+	} else if (r < 0.5) {
+		let bgCol = randomFill();
+		arc(x, y + h, w * 2, h * 2, -PI / 2, 0);
+		chooseDiffThen(bgCol);
+		arc(x + w, y, w * 2, h * 2, PI / 2, PI);
+	} else if (r < 0.75) {
+		let bgCol = randomFill();
+		arc(x + w, y, w * 2, h * 2, PI / 2, PI);
+		chooseDiffThen(bgCol);
+		arc(x, y + h, w * 2, h * 2, -PI / 2, 0);
+	} else {
+		let bgCol = randomFill();
+		arc(x, y, w * 2, h * 2, 0, PI / 2);
+		chooseDiffThen(bgCol);
+		arc(x + w, y + h, w * 2, h * 2, PI, (3 * PI) / 2);
+	}
+}
